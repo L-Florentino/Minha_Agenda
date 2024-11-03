3bc5255c94ebc109292d76a5a0658f55b37e2dfc
L-Florentino <florentinolucas097@gmail.com>
2024-11-03 19:32:49 -0300

    Tela de carregamento

--- app/src/main/java/com/example/minha_agenda/Tela_De_Carregamento.java
+++ app/src/main/java/com/example/minha_agenda/Tela_De_Carregamento.java
@@ -1,6 +1,8 @@
 package com.example.minha_agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,17 @@ public class Tela_De_Carregamento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_de_carregamento);
+
+        int Tempo = 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Tela_De_Carregamento.this, MainActivity.class));
+                finish();
+            }
+        },Tempo);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
