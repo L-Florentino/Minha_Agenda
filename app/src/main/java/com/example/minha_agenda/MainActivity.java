9b713414fb7dba598af5da95772576fc42887925
L-Florentino <florentinolucas097@gmail.com>
2024-11-03 20:47:44 -0300

    Login e Cadastro

--- app/src/main/java/com/example/minha_agenda/MainActivity.java
+++ app/src/main/java/com/example/minha_agenda/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.minha_agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Btn_Login, Btn_Registro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Btn_Login = findViewById(R.id.Btn_Login);
+        Btn_Registro = findViewById(R.id.Btn_Registro);
+
+        Btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
+        Btn_Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
+            }
         });
     }
 }
