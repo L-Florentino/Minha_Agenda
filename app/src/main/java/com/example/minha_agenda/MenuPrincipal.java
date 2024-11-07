538e1f16b9ac85e635156f773f98b416cc2de756
L-Florentino <florentinolucas097@gmail.com>
2024-11-06 22:13:59 -0300

    Botão sair (Encerrar sessão)

--- app/src/main/java/com/example/minha_agenda/MenuPrincipal.java
+++ app/src/main/java/com/example/minha_agenda/MenuPrincipal.java
@@ -1,6 +1,11 @@
 package com.example.minha_agenda;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +13,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuPrincipal extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MenuPrincipal extends AppCompatActivity {
+    
+    Button EncerrarSessao;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_principal);
+        
+        EncerrarSessao = findViewById(R.id.EncerrarSessao);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        
+        EncerrarSessao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SairApp();
+            }
+        });
+    }
+
+    private void SairApp() {
+        firebaseAuth.signOut();
+        startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+        Toast.makeText(this, "Você saiu com sucesso", Toast.LENGTH_SHORT).show();
     }
 }
